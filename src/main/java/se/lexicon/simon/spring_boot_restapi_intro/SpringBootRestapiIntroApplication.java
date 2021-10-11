aa3222a0d5ad4cfb5528792efd3e5b1391bd1e02
Simon Elbrink <simon.elbrink@gmail.com>
2021-10-11 12:25:07 +0200

    Init Commit

---
+++ src/main/java/se/lexicon/simon/spring_boot_restapi_intro/SpringBootRestapiIntroApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.simon.spring_boot_restapi_intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestapiIntroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestapiIntroApplication.class, args);
+    }
+
+}
